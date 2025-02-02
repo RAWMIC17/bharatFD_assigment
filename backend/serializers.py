647f266587f3895281d3915717ddbc82dda24021
RAWMIC17 <hks1710harsh@gmail.com>
2025-02-02 07:18:04 +0530

    fixed translations

--- backend/serializers.py
+++ backend/serializers.py
@@ -5,3 +5,13 @@ class FAQSerializer(serializers.ModelSerializer):
     class Meta:
         model = FAQ
         fields = ['id', 'question', 'answer']
+
+    def to_representation(self, instance):
+        lang = self.context.get('request').query_params.get('lang', 'en')
+        data = super().to_representation(instance)
+
+        if lang == 'hi':
+            data['question'] = getattr(instance, f'question_{lang}', instance.question)
+            data['answer'] = getattr(instance, f'answer_{lang}', instance.answer)
+
+        return data
